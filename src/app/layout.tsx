5a6be78e7d3e869eeab4a19389b6a2543cfd7bf9
Olivia <oliviaclarkeedwards@gmail.com>
2025-07-19 08:25:34 +1000

    add next and update file structure accordingly

---
+++ src/app/layout.tsx
@@ -0,0 +1,28 @@
+import { ReactNode } from 'react'
+import './global.css'
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <html className="no-js" lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Cafe Santosha - A hidden gem in Port Chalmers</title>
+        <link rel="stylesheet" type="text/css" href="@styles/style.css" />
+        <meta
+          name="description"
+          content="Come enjoy rustic ambiance of this hidden gem.  At Cafe Santosha in Port Chalmers you will find warm welcoming staff, Good Fortune coffee and a cabinet bursting with freshly baked goods."
+        />
+
+        <meta
+          property="og:title"
+          content="Cafe Santosha - A hidden gem in Port Chalmers"
+        />
+
+        <link rel="icon" href="favicon.ico" sizes="any" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+      </head>
+      <body>{children}</body>
+    </html>
+  )
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,44 @@
+export default function Home() {
+  return (
+    <>
+      <div className=".content-wrapper">
+        <img src="/cafe.jpg" alt="Cafe Santosha" />
+        <header>
+          <h1>Cafe Santosha</h1>
+          <div className="definition">
+            <h2 className="definition">santosha</h2>
+            <p>skt. संतोष saṃtoṣa</p>
+            <p>noun</p>
+            <p>deep, abiding, everlasting contentment.</p>
+          </div>
+        </header>
+        <div className="content">
+          <p>
+            Enjoy the rustic ambiance of this hidden gem, warm welcoming staff,
+            <br />
+            Good Fortune coffee and a cabinet bursting with freshly baked goods.
+          </p>
+          <p>
+            <i>Available for functions and events.</i>
+          </p>
+          <footer className="contact-details">
+            <a
+              href="https://www.google.com/maps/place/Cafe+Santosha/@-45.8160219,170.6209632,17z/data=!3m1!4b1!4m6!3m5!1s0xa8294d4fb9e58ac5:0x5f410a1b27ada69f!8m2!3d-45.8160219!4d170.6209632!16s%2Fg%2F11cly85dhg?entry=ttu&g_ep=EgoyMDI1MDExNS4wIKXMDSoASAFQAw%3D%3D"
+              target="_blank"
+            >
+              36 George Street <br />
+              Port Chalmers
+              <br />
+              Down the alleyway
+            </a>
+            <a href="tel:+642108361524">0210 836 1524</a>
+
+            <a href="mailto:cafe_santosha@outlook.com?subject=Website%20enquiry?body=Hi%20Cafe%20Santosha%2C%0A%0A">
+              cafe_santosha@outlook.com
+            </a>
+          </footer>
+        </div>
+      </div>
+    </>
+  )
+}
